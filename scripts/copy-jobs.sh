e6a8820462838ea4f31daaa6cf5456a4f8609c7d
Joey Takeda <joeytakeda@users.noreply.github.com>
2024-10-18 10:33:58 -0700

    Update jobs

--- scripts/copy-jobs.sh
+++ scripts/copy-jobs.sh
@@ -1,6 +1,9 @@
 ## Simple script to copy over job configurations
 
 #!/bin/bash
+echo "Removing old jobs"
 git rm -r jobs
+echo "Copying new jobs"
 rsync -rmv --include='*/' --include="config.xml" --exclude="*" jenkins_home/jobs .
+echo "Staging jobs directory"
 git add jobs
