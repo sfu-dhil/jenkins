184c4e87538578f0339e1ea04e4582ee18bd3313
Andrew Gardener <andrew.e.gardener@gmail.com>
2025-02-07 12:59:30 -0800

    Remove jobs

--- scripts/copy-jobs.sh
+++
@@ -1,9 +0,0 @@
-## Simple script to copy over job configurations
-
-#!/bin/bash
-echo "Removing old jobs"
-git rm -r jobs
-echo "Copying new jobs"
-rsync -rmv --include='*/' --include="config.xml" --exclude="*" jenkins_home/jobs .
-echo "Staging jobs directory"
-git add jobs
